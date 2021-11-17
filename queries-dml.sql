774cadbda11fd2f84d21fb7034bf86a3de37169f
Willians Martins <professorwillians@gmail.com>
2021-11-17 16:03:30 -0300

    Update queries-dml.sql

--- queries-dml.sql
+++ queries-dml.sql
@@ -5,4 +5,13 @@ INSERT INTO QUIZ_PESSOA VALUES (null, 'Willians Martins');
 INSERT INTO QUIZ_PESSOA VALUES (null, 'Luis Felipe Martins');
 
 -- READ (buscar registros)
-SELECT * FROM QUIZ_PESSOA
+SELECT * FROM QUIZ_PESSOA;
+
+-- UPDATE (atualizar registros)
+UPDATE QUIZ_PESSOA 
+SET nome = 'Willians Martins'
+WHERE id = 3;
+
+-- DELETE (remover registros)
+DELETE FROM QUIZ_PESSOA
+WHERE id BETWEEN DATE() and DATE();
