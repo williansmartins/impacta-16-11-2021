098d708dabeea2572b8a1671bb6e948a0ec3f81b
Willians Martins <professorwillians@gmail.com>
2021-11-17 15:35:20 -0300

    Create queries-dml.sql

---
+++ queries-dml.sql
@@ -0,0 +1,8 @@
+-- DML
+-- CRUD
+-- CREATE (criar registros)
+INSERT INTO QUIZ_PESSOA VALUES (null, 'Willians Martins');
+INSERT INTO QUIZ_PESSOA VALUES (null, 'Luis Felipe Martins');
+
+-- READ (buscar registros)
+SELECT * FROM QUIZ_PESSOA
