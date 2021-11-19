44833ba9ba2952aefd0cdaac6fefc3b153114242
Willians Martins <professorwillians@gmail.com>
2021-11-19 12:43:22 -0300

    add codigo de api

---
+++ api/servidor.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+
+app.get("/batatinha", function(req, res){
+    res.json({ message: "batatinha 123" });
+});
+
+app.listen(3000, function(){
+    console.info("Servidor esta de pé!");
+});
