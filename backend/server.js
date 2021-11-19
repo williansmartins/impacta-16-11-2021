9a1bcf94254b3b8a4274fa58a88b41dfe3a7f5b9
Willians Martins <professorwillians@gmail.com>
2021-11-19 11:55:33 -0300

    add pasta backend e o server.js

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express"); //FRAMEWORK MVC
+const app = express();
+
+// parse requests of content-type - application/json
+app.use(express.json()); /* bodyParser.json() is deprecated */
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true })); /* bodyParser.urlencoded() is deprecated */
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
+// require("./routes/pessoa.routes.js")(app);
+
+// configurando porta e ouvindo requisicoes
+const PORTA = 8080;
+app.listen(PORTA, () => {
+    console.log(`Servidor rodando na porta ${PORTA}.`);
+});
