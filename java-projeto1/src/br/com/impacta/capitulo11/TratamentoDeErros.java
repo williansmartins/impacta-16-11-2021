0411b4137905e565ac47091463a6be1a3ed2b0b2
Willians Martins <professorwillians@gmail.com>
2021-11-24 12:05:07 -0300

    add conteudo

--- java-projeto1/src/br/com/impacta/capitulo11/TratamentoDeErros.java
+++ java-projeto1/src/br/com/impacta/capitulo11/TratamentoDeErros.java
@@ -3,7 +3,10 @@ package br.com.impacta.capitulo11;
 public class TratamentoDeErros {
 
 	public static void main(String[] args) {
-	
+		
+		int valor = 10;
+		int resultado = valor / 10;
+		System.out.println(resultado);
 
 	}
 
