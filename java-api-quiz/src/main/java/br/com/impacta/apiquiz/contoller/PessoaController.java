960eef63237ae1eeb52e660e613a5c7b7bbdecbf
Willians Martins <professorwillians@gmail.com>
2021-11-26 08:49:00 -0300

    criar lista de pessoas mock

--- java-api-quiz/src/main/java/br/com/impacta/apiquiz/contoller/PessoaController.java
+++ java-api-quiz/src/main/java/br/com/impacta/apiquiz/contoller/PessoaController.java
@@ -1,5 +1,8 @@
 package br.com.impacta.apiquiz.contoller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +14,21 @@ import br.com.impacta.apiquiz.model.Pessoa;
 public class PessoaController {
 
 	//GET localhost:8080/pessoa/a
-	@GetMapping("/a")
-	public Pessoa getA() {
+	@GetMapping("")
+	public List<Pessoa> getAll() {
 		Pessoa p1 = new Pessoa();
-		p1.setNome("Willians");
-		p1.setIdade(123);
-		return p1;
+		p1.setNome("Luis Miguel");
+		p1.setIdade(5);
+
+		Pessoa p2 = new Pessoa();
+		p2.setNome("Luis Felipe");
+		p2.setIdade(7);
+
+		List<Pessoa> lista = new ArrayList<Pessoa>();
+		lista.add(p1);
+		lista.add(p2);
+
+		return lista;
 	}
 
 }
