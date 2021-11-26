ec3aa79751cf39d7beab2fa9bd294d80e22436ea
Willians Martins <professorwillians@gmail.com>
2021-11-26 11:22:59 -0300

    add metodos extras

--- java-api-quiz/src/main/java/br/com/impacta/apiquiz/contoller/PessoaController.java
+++ java-api-quiz/src/main/java/br/com/impacta/apiquiz/contoller/PessoaController.java
@@ -1,44 +1,60 @@
 package br.com.impacta.apiquiz.contoller;
 
-import br.com.impacta.apiquiz.repository.PessoaRepository;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.impacta.apiquiz.model.Pessoa;
+import br.com.impacta.apiquiz.repository.PessoaRepository;
 
 @RestController
 @RequestMapping("/pessoa")
 public class PessoaController {
-	
-	private final PessoaRepository repository;
-	
-	PessoaController(PessoaRepository repository) {
-	    this.repository = repository;
-	  }
 
-	//GET localhost:8080/pessoa/a
+	private final PessoaRepository repository;
+
+	PessoaController(PessoaRepository repository) {
+		this.repository = repository;
+	}
+
 	@GetMapping("")
 	public List<Pessoa> getAll() {
-		
 		return repository.findAll();
-		
-//		Pessoa p1 = new Pessoa();
-//		p1.setNome("Luis Miguel");
-//		p1.setIdade(5);
-//
-//		Pessoa p2 = new Pessoa();
-//		p2.setNome("Luis Felipe");
-//		p2.setIdade(7);
-//
-//		List<Pessoa> lista = new ArrayList<Pessoa>();
-//		lista.add(p1);
-//		lista.add(p2);
-//
-//		return lista;
+	}
+
+	@GetMapping("/{id}")
+	Optional<Pessoa> one(@PathVariable Long id) {
+		return repository.findById(id);
+	}
+
+	@PostMapping("")
+	public Pessoa create(@RequestBody Pessoa entrada) {
+		return repository.save(entrada);
+	}
+
+	@DeleteMapping("/{id}")
+	void deletePessoa(@PathVariable Long id) {
+		repository.deleteById(id);
+	}
+
+	@PutMapping("/employees/{id}")
+	Pessoa replace(@RequestBody Pessoa novo, @PathVariable Long id) {
+
+		return repository.findById(id).map(objeto -> {
+			objeto.setNome(novo.getNome());
+			return repository.save(objeto);
+		}).orElseGet(() -> {
+			novo.setId(id);
+			return repository.save(novo);
+		});
 	}
 
 }
--- java-api-quiz/src/main/java/br/com/impacta/apiquiz/model/Pessoa.java
+++ java-api-quiz/src/main/java/br/com/impacta/apiquiz/model/Pessoa.java
@@ -27,7 +27,14 @@ public class Pessoa {
 		this.idade = idade;
 	}
 	
-	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	@Override
 	public int hashCode() {
