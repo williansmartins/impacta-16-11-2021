c5bd6e6406208d247a71626206b8b9308f5892ea
Willians Martins <professorwillians@gmail.com>
2021-11-26 09:39:36 -0300

    add metodo GetAll

--- java-api-quiz/src/main/java/br/com/impacta/apiquiz/contoller/PessoaController.java
+++ java-api-quiz/src/main/java/br/com/impacta/apiquiz/contoller/PessoaController.java
@@ -1,5 +1,8 @@
 package br.com.impacta.apiquiz.contoller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +13,31 @@ import br.com.impacta.apiquiz.model.Pessoa;
 @RequestMapping("/pessoa")
 public class PessoaController {
 
-	//GET localhost:8080/pessoa/a
-	@GetMapping("/a")
+	//GET localhost:8080/pessoa/2
+	@GetMapping("/2")
 	public Pessoa getA() {
 		Pessoa p1 = new Pessoa();
 		p1.setNome("Willians");
 		p1.setIdade(123);
 		return p1;
+	}
+	
+	//GET localhost:8080/pessoa
+	@GetMapping("")
+	public List<Pessoa> getAll() {
+		Pessoa p1 = new Pessoa();
+		p1.setNome("Willians");
+		p1.setIdade(123);
+		
+		Pessoa p2 = new Pessoa();
+		p2.setNome("Luis Felipe");
+		p2.setIdade(44);
+		
+		List<Pessoa> lista = new ArrayList<Pessoa>();
+		lista.add(p1);
+		lista.add(p2);
+		
+		return lista;
 	}
 
 }
