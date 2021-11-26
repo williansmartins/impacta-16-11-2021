10f4fdb30571416b8d235bca7c3fff8c701ca507
Willians Martins <professorwillians@gmail.com>
2021-11-26 16:00:30 -0300

    update delete

--- java-api-quiz/src/main/java/br/com/impacta/apiquiz/contoller/PessoaController.java
+++ java-api-quiz/src/main/java/br/com/impacta/apiquiz/contoller/PessoaController.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +36,17 @@ public class PessoaController {
 	@GetMapping("/{id}")
 	public <Pessoa>Optional getOne(@PathVariable int id) {
 		return repository.findById(id);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable int id) {
+		repository.deleteById(id);
+	}
+	
+	@PutMapping("/{id}")
+	public Pessoa update(@RequestBody Pessoa entrada, @PathVariable int id) {
+		entrada.setId(id);
+		return repository.save(entrada);
 	}
 
 }
