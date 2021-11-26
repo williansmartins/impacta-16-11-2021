ba35eb065163ddeb459172ce25264d3f8c1b67fd
Willians Martins <professorwillians@gmail.com>
2021-11-26 15:36:54 -0300

    add getOne

--- java-api-quiz/src/main/java/br/com/impacta/apiquiz/contoller/PessoaController.java
+++ java-api-quiz/src/main/java/br/com/impacta/apiquiz/contoller/PessoaController.java
@@ -1,10 +1,11 @@
 package br.com.impacta.apiquiz.contoller;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,17 +20,7 @@ public class PessoaController {
 	
 	@Autowired
 	PessoaRepository repository;
-
-	//GET localhost:8080/pessoa/2
-	@GetMapping("/1")
-	public Pessoa getA() {
-		Pessoa p1 = new Pessoa();
-		p1.setNome("Willians");
-		p1.setIdade(123);
-		return p1;
-	}
 	
-	//GET localhost:8080/pessoa
 	@GetMapping("")
 	public List<Pessoa> findAll() {
 		return repository.findAll();
@@ -38,6 +29,11 @@ public class PessoaController {
 	@PostMapping("")
 	public Pessoa create(@RequestBody Pessoa entrada) {
 		return repository.save(entrada);
+	}
+	
+	@GetMapping("/{id}")
+	public <Pessoa>Optional getOne(@PathVariable int id) {
+		return repository.findById(id);
 	}
 
 }
