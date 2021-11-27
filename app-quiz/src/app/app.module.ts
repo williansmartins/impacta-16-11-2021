1d88bbba19546c2182b31caf2a91c3f4eb200689
Willians Martins <professorwillians@gmail.com>
2021-11-27 19:26:05 -0300

    criacao do projeto

--- app-quiz/src/app/app.module.ts
+++ app-quiz/src/app/app.module.ts
@@ -1,40 +1,20 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatTableModule } from '@angular/material/table';
-
-import { LOCALE_ID } from '@angular/core';
-import { registerLocaleData } from '@angular/common';
-import localePt from '@angular/common/locales/pt';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { PessoasComponent } from './pessoas/pessoas.component';
-
-registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PessoasComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    MatTableModule, 
-    MatSnackBarModule,
+    BrowserAnimationsModule
   ],
-  providers: [
-    { provide: LOCALE_ID, useValue: 'pt-BR' }  
-    
-    // { provide: HTTP_INTERCEPTORS, useClass: CacheInterceptor, multi: true }
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
